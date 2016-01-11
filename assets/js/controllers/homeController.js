c6bb4466b1176218e1f1b4f4b1e42e432cafb5b8
Phelippe <phelippematte@gmail.com>
2016-01-11 12:01:45 -0300

    .

--- assets/js/controllers/homeController.js
+++ assets/js/controllers/homeController.js
@@ -1,21 +1,50 @@
 angular.module('app.controllers')
-    .controller('HomeController', ['$scope', 'Finder', function ($scope, Finder) {
+    .controller('HomeController', ['$scope', '$http', 'Finder', function ($scope, $http, Finder) {
         console.log('homeController');
+
+        //var token = 'j1xXo8WWivIQgbcj0eFBRJDNnsZuPPkoQL3MBBBmdEgqI3tZbb';
+        var token = 'YlgYmTP0dbWoHbIIQuxTioxCv89oMTfb9FlCejGFH4jbzPQuhP';
 
         $scope.buscar = function () {
             console.log('Buscando por: ' + $scope.tumblr.nome);
 
-            Finder.get({
-                name: $scope.tumblr.nome,
-            }, function(data){ // success
-                console.log(data);
-            })
+            /*var deferred = $q.defer();
+            $tst = $http.get('http://chrisburkard.tumblr.com/api/read/json')
+                .success(function (data, status, headers, config) {
+                    console.log(data);
+                    deferred.resolve(data);
+                    console.log('#fim#');
+                })
+                .error(function (data, status, headers, config) {
+                    console.log(status);
+                    defferd.reject(status);
+                });*/
 
-            /*if ($scope.form.$valid) {
-             $scope.project_task.$save({id_project: $routeParams.id_project}).then(function () {
-             $location.path('/projetos/' + $routeParams.id_project + '/tarefas');
-             });
-             }*/
+            $http.jsonp('http://api.tumblr.com/v2/blog/'+$scope.tumblr.nome+'.tumblr.com/posts?token='+token+'&jsonp=JSON_CALLBACK')
+                .success(function(data,status,headers,config){
+                    console.log(data);
+                })
+                .error(function(data,status,headers,config){
+                    console.log(data, status, headers, config);
+                });
+
+            /*Finder.get({
+                blog_name: $scope.tumblr.nome,
+                type: 'post',
+            }, function(data,status, headers){ // success
+                console.log(data,status, headers);
+            }, function(data,status, headers){//error
+                console.log(data,status, headers);
+            })*/
+
+            /*Finder.jsonp({
+                name: $scope.tumblr.nome,
+             }, function(data,status, headers){ // success
+                console.log(data,status, headers);
+             }, function(data,status, headers){//error
+                console.log(data,status, headers);
+            })*/
+
         }
 
     }]);
